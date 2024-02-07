9c0266d565617379597836b363949b083210d6d9
smariannee <20213tn140@utez.edu.mx>
2024-02-06 19:36:35 -0600

    avance design

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,14 @@
 <script setup>
-
+import Movies from "./modules/movies/views/Movies.vue";
+components: { Movies }
 </script>
 
 <template>
   <div id="app">
-    <header>
-    </header>
-
     <main>
+      <movies />
     </main>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
