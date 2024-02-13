9fd9c799fe80f35ebb928513052523975afd34c5
Marianne Santos <20213tn140@utez.edu.mx>
2024-02-12 20:04:59 -0600

    search and filter card design and other corrections

--- frontend/src/modules/movies/views/Movies.vue
+++ frontend/src/modules/movies/views/Movies.vue
@@ -7,7 +7,7 @@ export default {
                 name: '',
                 releaseDate: null,
                 category: {
-                    id: null
+                    id: ''
                 },
                 description: '',
             },
@@ -16,7 +16,7 @@ export default {
                 director: '',
                 startYear: null,
                 endYear: null,
-                category: null,
+                category: '',
                 order: 'default'
             },
             options: [],
@@ -81,11 +81,11 @@ export default {
                     break;
                 case 'startYear':
                     const inputStartYear = document.getElementById('startYear');
-                    if(!this.notThisYear(startYear)){
+                    if (!this.notThisYear(startYear)) {
                         inputStartYear.classList.add('is-invalid');
                         errorMessages.startYear = 'El año de inicio no puede ser mayor al año actual';
                         showErrors.startYear = true;
-                    } else if(startYear<0){
+                    } else if (startYear < 0) {
                         inputStartYear.classList.add('is-invalid');
                         errorMessages.startYear = 'El año de inicio no puede ser negativo';
                         showErrors.startYear = true;
@@ -98,20 +98,20 @@ export default {
 
                 case 'endYear':
                     const inputEndYear = document.getElementById('endYear');
-                    if(!this.notThisYear(endYear)){
+                    if (!this.notThisYear(endYear)) {
                         inputEndYear.classList.add('is-invalid');
                         errorMessages.endYear = 'El año de fin no puede ser mayor al año actual';
                         showErrors.endYear = true;
-                    }else if(endYear<0){
+                    } else if (endYear < 0) {
                         inputEndYear.classList.add('is-invalid');
                         errorMessages.endYear = 'El año de fin no puede ser negativo';
                         showErrors.endYear = true;
-                    }else if(endYear<startYear){
+                    } else if (endYear < startYear) {
                         inputEndYear.classList.add('is-invalid');
                         errorMessages.endYear = 'El año de fin no puede ser menor al año de inicio';
                         showErrors.endYear = true;
-                    
-                    }else {
+
+                    } else {
                         inputEndYear.classList.remove('is-invalid');
                         errorMessages.endYear = '';
                         showErrors.endYear = false;
@@ -170,6 +170,27 @@ export default {
             }).catch((error) => {
                 alert("Error al agregar la película");
             });
+        },
+        cleanFilters() {
+            this.filterDto = {
+                name: '',
+                director: '',
+                startYear: null,
+                endYear: null,
+                category: "",
+                order: 'default'
+            }
+            this.getMovies();
+        },
+        resetForm() {
+            this.form = {
+                name: '',
+                releaseDate: null,
+                category: {
+                    id: ''
+                },
+                description: '',
+            }
         }
     },
     mounted() {
@@ -180,130 +201,185 @@ export default {
 </script>
 
 <template>
-    <b-container fluid class="px-5">    
+    <b-container fluid class="px-5">
         <b-row>
-            <b-col cols="12" md="9" class="mt-5">
-                <h4>Películas</h4>
+            <b-col cols="12" class="mt-5 px-3">
+                <b-form>
+                    <b-card>
+                        <template #header>
+                            <h5 class="mb-0">Filtros avanzados</h5>
+                        </template>
+                        <b-row>
+                            <b-col cols="12" md="4">
+                                <b-form-group label="Buscar por nombre:" label-for="name">
+                                    <b-form-input id="name" type="text" v-model.trim="filterDto.name"
+                                        @keyup.enter="getMovies()"></b-form-input>
+                                </b-form-group>
+                                <b-form-group label="Buscar por director:" label-for="name" class="mt-3">
+                                    <b-form-input id="director" type="text" v-model.trim="filterDto.director"
+                                        @keyup.enter="getMovies()"></b-form-input>
+                                </b-form-group>
+                            </b-col>
+                            <b-col cols="12" md="4">
+                                <b-form-group label="Entre el año:" label-for="startYear" class="mt-3 mt-md-0"
+                                    :state="!showErrors.startYear" :invalid-feedback="errorMessages.startYear">
+                                    <b-form-input id="startYear" @input="validateInput('startYear')" type="number"
+                                        v-model.trim="filterDto.startYear"></b-form-input>
+                                </b-form-group>
+                                <b-form-group label="Y el año:" label-for="endYear" class="mt-3"
+                                    :state="!showErrors.endYear" :invalid-feedback="errorMessages.endYear">
+                                    <b-form-input id="endYear" @input="validateInput('endYear')" type="number"
+                                        v-model.trim="filterDto.endYear"></b-form-input>
+                                </b-form-group>
+                            </b-col>
+                            <b-col cols="12" md="4">
+                                <b-form-group label="Buscar por categoría:" label-for="category" class="mt-3 mt-md-0">
+                                    <b-form-select id="category" v-model.trim="filterDto.category" class="input-select">
+                                        <option value="">Todas las categorías</option>
+                                        <option v-for="option in options" :key="option.id" :value="option.id">
+                                            {{ option.name }}
+                                        </option>
+                                    </b-form-select>
+                                </b-form-group>
+                                <b-form-group label="Ordenar por:" label-for="order" class="mt-3">
+                                    <b-form-select id="order" v-model.trim="filterDto.order"
+                                        :options="[{ text: 'Predeterminado', value: 'default' }, { text: 'Ascendente', value: 'asc' }, { text: 'Descendente', value: 'desc' }]"
+                                        class="input-select">
+                                    </b-form-select>
+                                </b-form-group>
+                            </b-col>
+                            <b-col cols="6" md="4" class="mt-3">
+                                <b-button variant="outline-primary" @click="getMovies()" style="width: 100%;"
+                                    :disabled="showErrors.startYear || showErrors.endYear"
+                                    class="d-flex justify-content-between">
+                                    Buscar y aplicar filtros
+                                    <b-icon icon="search"></b-icon>
+                                </b-button>
+                            </b-col>
+                            <b-col cols="6" md="4" class="mt-3">
+                                <b-button variant="outline-danger" type="reset" @click="cleanFilters()" style="width: 100%;"
+                                    class="d-flex justify-content-between">
+                                    Limpiar
+                                    <b-icon icon="trash"></b-icon>
+                                </b-button>
+                            </b-col>
+                        </b-row>
+                    </b-card>
+                </b-form>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col cols="12" md="9">
+                <h3 class="mt-5">Catálogo de películas disponibles</h3>
             </b-col>
             <b-col cols="12" md="3" class="mt-3 mt-md-5">
-                <b-button v-b-modal.modal-1 variant="outline-success" class="position-right">
-                    Agregar nueva película
+                <b-button variant="outline-success" @click="$bvModal.show('modal-1')" style="width: 100%;"
+                    class="d-flex justify-content-between">
+                    Agregar película
                     <b-icon icon="plus-circle"></b-icon>
                 </b-button>
             </b-col>
         </b-row>
-        <b-form>
-            <b-row>
-                <b-col cols="3">
-                    <!--Input Buscardor -->
-                    <b-form-group label="Buscar por Nombre:" label-for="name" class="mt-1">
-                        <b-form-input id="name" type="text" v-model.trim="filterDto.name" @keyup.enter="getMovies()"></b-form-input>
-                    </b-form-group>
-                    <b-form-group label="Buscar por Director:" label-for="name" class="mt-1">
-                        <b-form-input id="director" type="text" v-model.trim="filterDto.director" @keyup.enter="getMovies()"></b-form-input>
-                    </b-form-group>
-                </b-col>
-                <b-col cols="3">
-                    <!--Input Año de estreno -->
-                    <b-form-group label="Entre año:" label-for="startYear" class="mt-1"
-                        :state="!showErrors.startYear" :invalid-feedback="errorMessages.startYear">
-                        <b-form-input id="startYear" @input="validateInput('startYear')"
-                        type="number" v-model.trim="filterDto.startYear"></b-form-input>
-                    </b-form-group>
-
-                    <b-form-group label="Y año:" label-for="endYear" class="mt-1"
-                        :state="!showErrors.endYear" :invalid-feedback="errorMessages.endYear">
-                        <b-form-input id="endYear" @input="validateInput('endYear')"
-                        type="number" v-model.trim="filterDto.endYear"></b-form-input>
-                    </b-form-group>
-                </b-col>
-                <b-col cols="3">
-                    <!--Input Categoría -->
-                    <b-form-group label="Categoría:" label-for="category" class="mt-1 input-select">
-                        <b-form-select id="category" v-model.trim="filterDto.category"
-                            :options="options.map(option => ({ text: option.name, value: option.id }))"
-                            style="width: 100%;"></b-form-select>
-                    </b-form-group>
-                    <!--Input Ordenar por -->
-                    <b-form-group label="Orden:" label-for="order" class="mt-1 input-select">
-                        <b-form-select id="order" v-model.trim="filterDto.order"
-                            :options="[{ text: 'Default', value: 'default' }, { text: 'Ascendente', value: 'asc' }, { text: 'Descendente', value: 'desc' }]"
-                            style="width: 100%;"></b-form-select>
-                    </b-form-group>
-                </b-col>
-                <b-col cols="3">
-                    <b-button class="mt-3" variant="outline-primary" @click="getMovies()" style="width: 70%;"
-                        :disabled="showErrors.startYear || showErrors.endYear"
-                    >
-                    Buscar
-                    <b-icon icon="search"></b-icon>
-                </b-button>
-                </b-col>
-            </b-row>
-        </b-form>
         <b-row>
-            <b-col cols="12">
-                <b-row>
-                    <b-col v-for="movie in movies" :key="movie.id" cols="3" md="3" class="mt-3">
-                        <b-card class="shadow-sm mb-2" :title="movie.name" :img-src="movie.image" img-alt="Image" img-top
-                            tag="article" style="max-width: 20rem; min-height: 15rem;" :sub-title="movie.director + ' -> ' + movie.category.name"
-                            onmouseover="this.style.transform='scale(1.1)'" onmouseout="this.style.transform='scale(1)'">
-                            <b-card-text>
-                                {{ movie.description }}
-                            </b-card-text>
-                            <template #footer>
-                                <p> <b-icon icon="calendar-date"></b-icon>
-                                    Fecha de estreno: <strong>{{ movie.releaseDate }}</strong></p>
-                            </template>
-                        </b-card>
-                    </b-col>
-                    <img src="../../../assets/noMovies.png" alt="notFound" v-if="movies.length == 0">
-
-                </b-row>
+            <b-col v-for="movie in movies" :key="movie.id" cols="12" md="6" lg="3" class="mt-5">
+                <b-card class="shadow-sm mb-2" :title="movie.name" onmouseover="this.style.transform='scale(1.1)'"
+                    onmouseout="this.style.transform='scale(1)'">
+                    <h6>{{ movie.director }}</h6>
+                    <b-card-sub-title>
+                        {{ movie.category.name }}
+                    </b-card-sub-title>
+                    <b-card-text class="mt-2">
+                        {{ movie.description }}
+                    </b-card-text>
+                    <template #footer>
+                        <p>
+                            <b-icon icon="calendar"></b-icon>
+                            Fecha de estreno: <strong>{{ movie.releaseDate }}</strong>
+                        </p>
+                    </template>
+                </b-card>
+            </b-col>
+            <b-col cols="12" class="my-5 d-flex justify-content-center" v-if="movies.length == 0">
+                <img src="../../../assets/noMovies.png" alt="Imágenes no encontradas" class="img">
             </b-col>
         </b-row>
         <b-modal hide-footer id="modal-1" title="Agregar nueva película">
             <b-form>
                 <b-row>
-                    <b-col cols="6">
+                    <b-col cols="12">
                         <b-form-group label="Título:" label-for="name" :state="!showErrors.name"
                             :invalid-feedback="errorMessages.name">
                             <b-form-input id="name" type="text" v-model.trim="form.name"
                                 @input="validateInput('name')"></b-form-input>
                         </b-form-group>
                     </b-col>
-                    <b-col cols="6">
+                    <b-col cols="12" class="mt-2">
+                        <b-form-group label="Director:" label-for="director">
+                            <b-form-input id="director" type="text" v-model.trim="form.director"></b-form-input>
+                        </b-form-group>
+                    </b-col>
+                    <b-col cols="12" class="mt-2">
                         <b-form-group label="Año de estreno:" label-for="releaseDate" :state="!showErrors.releaseDate"
                             :invalid-feedback="errorMessages.releaseDate">
                             <b-form-input @input="validateInput('releaseDate')" id="releaseDate" type="number"
                                 v-model.trim="form.releaseDate"></b-form-input>
                         </b-form-group>
                     </b-col>
-                    <b-col cols="12">
-                        <b-form-group label-for="category" class="mt-3 input-select">
+                    <b-col cols="12" class="mt-4">
+                        <b-form-group label-for="category">
                             <b-form-select id="category" v-model.trim="form.category.id" @input="validateInput('category')"
                                 :state="!showErrors.category" :invalid-feedback="errorMessages.category"
                                 :options="options.map(option => ({ text: option.name, value: option.id }))"
-                                style="width: 100%;"></b-form-select>
+                                class="input-select">
+                                <option value="" disabled>Selecciona una categoría...</option>
+                            </b-form-select>
                         </b-form-group>
                     </b-col>
-                    <b-col cols="12">
-                        <b-form-group label="Sinopsis:" label-for="description" class="mt-3">
+                    <b-col cols="12" class="mt-4">
+                        <b-form-group label="Sinopsis:" label-for="description">
                             <b-form-textarea id="description" @input="validateInput('description')"
                                 :state="!showErrors.description" :invalid-feedback="errorMessages.description"
                                 v-model.trim="form.description"></b-form-textarea>
                         </b-form-group>
                     </b-col>
                     <b-col cols="6">
-                        <b-button class="mt-3" variant="outline-danger" type="reset" style="width: 100%;">Limpiar</b-button>
+                        <b-button class="mt-4 d-flex justify-content-between" variant="outline-danger" type="reset" @click="resetForm()" style="width: 100%;">
+                            Limpiar
+                            <b-icon icon="trash" font-scale="1"></b-icon>
+                        </b-button>
                     </b-col>
                     <b-col cols="6">
-                        <b-button block class="mt-3" variant="outline-success" type="submit" @click="addMovie()"
+                        <b-button block class="mt-4 d-flex justify-content-between" variant="outline-success" type="submit" @click="addMovie()"
                             :disabled="(showErrors.name || showErrors.releaseDate || showErrors.category || showErrors.description)"
-                            style="width: 100%;">Agregar</b-button>
+                            style="width: 100%;">
+                            Agregar
+                            <b-icon icon="plus-circle" font-scale="1"></b-icon>
+                        </b-button>
                     </b-col>
                 </b-row>
             </b-form>
         </b-modal>
     </b-container>
 </template>
+
+<style scoped>
+.input-select {
+    width: 100%;
+    border: 1px solid #ced4da;
+    border-radius: 0.25rem;
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    background-color: #fff;
+    background-clip: padding-box;
+    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+
+.img {
+    width: 360px;
+}
+
+.btn {
+    width: 100%;
+}
+</style>
